434a6480490aa8f75ccd6c9dfb240c90946b25ae
Lean 4 VS Code Extension <>
2025-09-23 20:09:13 -0400

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import StartLean
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ StartLean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `StartLean` library.
+-- Import modules here that should be built as part of the library.
+import StartLean.Basic
